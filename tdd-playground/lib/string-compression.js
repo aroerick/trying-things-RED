f976c093513b4db2933dff95e79057f7bd4ae6df
aroerick <a.roerick@gmail.com>
2018-08-14 10:06:15 -0700

    Add check if input is string.

--- tdd-playground/lib/string-compression.js
+++ tdd-playground/lib/string-compression.js
@@ -1,15 +1,19 @@
 module.exports = comp => {
-  compArr = comp.replace(/[\s\W] /g, '').split('')
-
-  compObj = {}
-
-  for (let i = 0; i < compArr.length; i++) {
-    let num = compArr[i]
-    compObj[num] = compObj[num] ? compObj[num] + 1 : 1
+  if(typeof comp === 'string') {
+    compArr = comp.replace(/[\s\W] /g, '').split('')
+   
+     compObj = {}
+   
+     for (let i = 0; i < compArr.length; i++) {
+       let num = compArr[i]
+       compObj[num] = compObj[num] ? compObj[num] + 1 : 1
+     }
+     const arrObj = Object.entries(compObj)
+     const comped = [].concat.apply([], arrObj).join('')
+   
+     return comped
+  } else {
+    return 'error'
   }
-  const arrObj = Object.entries(compObj)
-  const comped = [].concat.apply([], arrObj).join('')
-
-  return comped
   console.log([].concat.apply([], arrObj).join(''))
 }
