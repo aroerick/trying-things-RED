dc0649db0195578c49df2258748190c59cacb52a
aroerick <a.roerick@gmail.com>
2018-08-14 09:32:04 -0700

    Create jest tests for string-compression.

---
+++ tdd-playground/__tests__/string-compression.spec.js
@@ -0,0 +1,10 @@
+const comp = require('../lib/string-compression')
+
+describe('stringCompression', () => {
+    decribe('when there are multiple letters in a row', () => {
+        it('should return that letter once and the number of times it appears', () => {
+            const result = comp()
+            expect(result).toEqual()
+        })
+    })
+})
