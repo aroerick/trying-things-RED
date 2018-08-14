414862badb9bbc120b144c4c79023d957266dfe3
aroerick <a.roerick@gmail.com>
2018-08-14 11:26:26 -0700

    Refactor to pass non-sequential test.

--- tdd-playground/__tests__/string-compression.spec.js
+++ tdd-playground/__tests__/string-compression.spec.js
@@ -3,8 +3,8 @@ const comp = require('../lib/string-compression')
 describe('stringCompression', () => {
     describe('when there are multiple letters in a row', () => {
         it('should return that letter once and the number of times it appears', () => {
-            const result = comp('aaaabbbcc')
-            expect(result).toEqual('a4b3c2')
+            const result = comp('rrdd')
+            expect(result).toEqual('r2d2')
         })
     })
     describe('when a letter appears once', () => {
@@ -15,8 +15,8 @@ describe('stringCompression', () => {
     })
     describe('when there are non letter characters', () => {
         it('should error', () => {
-            const result = comp('abc123')
-            expect(result).toEqual('error')
+            const result = comp('a bbb c123!')
+            expect(result).toEqual('a1b3c1')
         })
     })
     describe('when input is not a string', () => {
@@ -35,6 +35,18 @@ describe('stringCompression', () => {
         it('should treat them as separate', () => {
             const result = comp('aAbb')
             expect(result).toEqual('a1A1b2')
+        })
+    })
+    describe('when called without an argument', () => {
+        it('should error', () => {
+            const result = comp()
+            expect(result).toEqual('error')
+        })
+    })
+    describe('when called without an empty string', () => {
+        it('should error', () => {
+            const result = comp('')
+            expect(result).toEqual('error')
         })
     })
 })
--- tdd-playground/lib/string-compression.js
+++ tdd-playground/lib/string-compression.js
@@ -1,22 +1,49 @@
-module.exports = comp => {
-  if(typeof comp === 'string') {
-    compArr = comp.replace(/[\s\W] /g, '').split('')
-   
-     compObj = {}
-   
-     for (let i = 0; i < compArr.length; i++) {
-       let num = compArr[i]
-       if(num.match(/[a-z]/i)){
-         compObj[num] = compObj[num] ? compObj[num] + 1 : 1
-       } else {
-         return 'error'
-       }
-     }
-     const arrObj = Object.entries(compObj)
-     const comped = [].concat.apply([], arrObj).join('')
-   
-     return comped
-  } else {
-    return 'error'
+// module.exports = comp => {
+//   if (typeof comp === 'string') {
+//     compArr = comp.replace(/[\s\W] /g, '').split('')
+
+//     compObj = {}
+
+//      for (let i = 0; i < compArr.length; i++) {
+//        let num = compArr[i]
+//        if(num.match(/[a-z]/i)){
+//          compObj[num] = compObj[num] ? compObj[num] + 1 : 1
+//        } else {
+//          return 'error'
+//        }
+//      }
+//     // compArr.forEach(char => {
+//     //   console.log(compArr.indexOf(char))
+//     //   if (true) {
+//     //     compObj[char] = compObj[char] ? compObj[char] + 1 : 1
+//     //   } else {
+//     //     return 'error'
+//     //   }
+//     // })
+
+//     if (Object.keys(compObj) === []){
+//       return 'error'
+//     } else {
+//       const arrObj = Object.entries(compObj)
+//       const comped = [].concat.apply([], arrObj).join('')
+//       console.log(Object.keys(compObj))
+//       return comped
+//     }
+//   } else {
+//     return 'error'
+//   }
+// }
+module.exports = str => {
+  let output = ''
+  let count = 0
+  if (typeof str !== 'string' || str === '') return 'error'
+  for (let i = 0; i < str.length; i++) {
+    count++
+    newStr = str.replace(/[\s\W\0-9]/g, '')
+    if (newStr[i] != newStr[i + 1]) {
+      output += newStr[i] + count
+      count = 0
+    }
   }
+  return output
 }
