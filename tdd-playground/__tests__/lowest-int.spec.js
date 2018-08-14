e23b11d19b98ba5fc090be2798f09000f3584470
aroerick <a.roerick@gmail.com>
2018-08-14 16:54:56 -0700

    Add additional tests and fix lowest-int to pass.

--- tdd-playground/__tests__/lowest-int.spec.js
+++ tdd-playground/__tests__/lowest-int.spec.js
@@ -3,13 +3,13 @@ const low = require('../lib/lowest-int')
 describe('lowestInteger', () => {
   describe('when given array', () => {
     it('should return the lowest number greater than 0 and not in the array', () => {
-      const result = low([2, 3, 21, 4])
+      const result = low([0, 2, 3, 21, 4])
       expect(result).toEqual(1)
     })
   })
   describe('when given array with duplicate values', () => {
     it('should return the lowest number greater than 0 and not in the array', () => {
-      const result = low([0, 1, 2, 3, 21, 1, 4, 4])
+      const result = low([1, 2, 3, 21, 1, 4, 4])
       expect(result).toEqual(5)
     })
   })
@@ -35,6 +35,44 @@ describe('lowestInteger', () => {
     it('should error', () => {
       const result = low()
       expect(result).toEqual('error')
+    })
+  })
+
+
+  describe('When array length is zero', () => {
+    it('should return 0', () => {
+      const result = low([])
+      expect(result).toEqual('error')
+    })
+  })
+  describe('if array is not passed in', () => {
+    it('should return 0', () => {
+      const result = low('hello')
+      expect(result).toEqual('error')
+    })
+  })
+  describe('if array is passed with elements that are  not integers', () => {
+    it('should return 0', () => {
+      const result = low([1, 3, 5, 'hello'])
+      expect(result).toEqual('error')
+    })
+  })
+  describe('all array elements are negative', () => {
+    it('should return 1', () => {
+      const result = low([-1, 0, -2, -3])
+      expect(result).toEqual(1)
+    })
+  })
+  describe('if array contains all the same number', () => {
+    it('should return a number 1 lower than it unless it would equal zero', () => {
+      const result = low([1, 1, 1, 1, 1])
+      expect(result).toEqual(2)
+    })
+  })
+  describe('if array contains valid positive integers', () => {
+    it('should return smallest positive number', () => {
+      const result = low([1, 2, 3, 4, 6, 1])
+      expect(result).toEqual(5)
     })
   })
 })
--- tdd-playground/lib/lowest-int.js
+++ tdd-playground/lib/lowest-int.js
@@ -1,15 +1,41 @@
 module.exports = arr => {
   if (!arr || !Array.isArray(arr) || !arr.length) return 'error'
-  let sort = arr.sort((a, b) => a - b)
 
-  let unique = []
-  for (let i = 0; i < sort.length; i++) {
-    if (unique.indexOf(sort[i]) === -1) unique.push(sort[i])
+  let result = [];
+
+  for (let i = 0; i < arr.length; ++i) {
+    if(typeof arr[i] !== 'number') return 'error'
+    if (0 <= arr[i]) {
+      result[arr[i]] = true;
+    }
   }
-  let count = 0
-  for (let i = 0; i < unique.length; i++) {
-    if (typeof unique[i] !== 'number') return 'error'
-    if (count !== unique[i] && count > 0) return count
-    count++
+
+  for (let i = 1; i <= result.length; ++i) {
+    if (undefined === result[i]) {
+      return i;
+    }
   }
+
+  return 1
+
+  // const set = new Set(arr)
+  // let i = 1
+  // while (set.has(i)) {
+  //   i++
+  // }
+  // return i
+
+  // let sort = arr.sort((a, b) => a - b)
+
+  // let unique = []
+  // for (let i = 0; i < sort.length; i++) {
+  //   if (unique.indexOf(sort[i]) === -1) unique.push(sort[i])
+  // }
+  // if(unique.includes(1)) return 1
+  
+  // let count = 0
+  // for (let i = 0; i < unique.length; i++) {
+  //   count++
+  //   if (count !== unique[i]) return count
+  // }
 }
