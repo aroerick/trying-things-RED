7fc6d7e899c2b431527400a2cc9b8d343b1fe99b
aroerick <a.roerick@gmail.com>
2018-08-14 14:35:05 -0700

    Create tests for lowest-int function.

---
+++ tdd-playground/__tests__/lowest-int.spec.js
@@ -0,0 +1,22 @@
+const low = require('../lib/lowest-int')
+
+describe('lowestInteger', () => {
+  describe('when given array', () => {
+    it('should return the lowest number greater than 0 and not in the array', () => {
+      const result = low([1, 3, 4])
+      expect(result).toEqual(2)
+    })
+  })
+  describe('when input is not an array', () => {
+    it('should error', () => {
+      const result = low('pickle')
+      expect(result).toEqual('error')
+    })
+  })
+  describe('when given array with non-number indexes', () => {
+    it('should error', () => {
+      const result = low([1, 'apple', {}])
+      expect(result).toEqual('error')
+    })
+  })
+})
