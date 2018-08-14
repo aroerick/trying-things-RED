72588675e1ad0aa58341bf215056876074b35772
aroerick <a.roerick@gmail.com>
2018-08-14 14:12:58 -0700

    Remove commented code and spacing in tdd-playground.

--- tdd-playground/lib/string-compression.js
+++ tdd-playground/lib/string-compression.js
@@ -1,38 +1,3 @@
-// module.exports = comp => {
-//   if (typeof comp === 'string') {
-//     compArr = comp.replace(/[\s\W] /g, '').split('')
-
-//     compObj = {}
-
-//      for (let i = 0; i < compArr.length; i++) {
-//        let num = compArr[i]
-//        if(num.match(/[a-z]/i)){
-//          compObj[num] = compObj[num] ? compObj[num] + 1 : 1
-//        } else {
-//          return 'error'
-//        }
-//      }
-//     // compArr.forEach(char => {
-//     //   console.log(compArr.indexOf(char))
-//     //   if (true) {
-//     //     compObj[char] = compObj[char] ? compObj[char] + 1 : 1
-//     //   } else {
-//     //     return 'error'
-//     //   }
-//     // })
-
-//     if (Object.keys(compObj) === []){
-//       return 'error'
-//     } else {
-//       const arrObj = Object.entries(compObj)
-//       const comped = [].concat.apply([], arrObj).join('')
-//       console.log(Object.keys(compObj))
-//       return comped
-//     }
-//   } else {
-//     return 'error'
-//   }
-// }
 module.exports = str => {
   let output = ''
   let count = 0
--- tdd-playground/lib/unique-char.js
+++ tdd-playground/lib/unique-char.js
@@ -1,14 +1,13 @@
 module.exports = str => {
   if ( !str || typeof str !== 'string' || !str.length ) return 'error'
 
-  strArr = str.replace(/[\s\W\d]+/g, '').split('')
-
+  strArr = str.replace(/[\s\W\d]/g, '').split('')
   strObj = {}
+
   for (let i = 0; i < strArr.length; i++) {
     let char = strArr[i]
     strObj[char] = strObj[char] ? strObj[char] + 1 : 1
   }
   result = Object.keys(strObj).find(key => strObj[key] === 1)
-
   return result
 }
